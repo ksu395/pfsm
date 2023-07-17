8784b8f3d09f4f50576f560b7eb382818cc29b6d
ksu395 <ksu395@icloud.com>
2023-07-17 01:11:38 +0000

    loosen allclose tolerance

--- tn.py
+++ tn.py
@@ -4,8 +4,8 @@ import torchtext
 
 torch.manual_seed(13)
 
-seq_len = 8
-embed_dim = 3
+seq_len = 128
+embed_dim = 100
 
 query = torch.randn(seq_len, embed_dim)
 key = torch.randn(seq_len, embed_dim)
@@ -16,7 +16,7 @@ value = torch.randn(seq_len, embed_dim)
 # Specifically section 3.2.1 and figure 2
 # For simplicity. this code does not consider the needed backward function
 # For simplicity. this code does not consider batch > 1
-def partially_fused_softmax(qkt, v) -> torch.tensor:
+def partially_fused_softmax(qkt: torch.tensor, v: torch.tensor) -> torch.tensor:
     # functionally, this code performs the following operations:
     #
     # scores = torch.nn.functional.softmax(qkt, dim=-1)
@@ -31,31 +31,31 @@ def partially_fused_softmax(qkt, v) -> torch.tensor:
     # with the size needed for a local gemm engine
 
     # compute the denominator, one per row
-    outsize = [qkt.size(-2), v.size(-1)]
+    nrows, ncols = qkt.size(-2), v.size(-1)
     qkti_max = qkt.max(-1)[0]  # ignore indices
-    sum_of_exp = [np.exp(qkt[i] - qkti_max[i]).sum().item() for i in range(outsize[0])]
+    sum_of_exp = [np.exp(qkt[i] - qkti_max[i]).sum().item() for i in range(nrows)]
 
     # compute the matmul, one output pixel at a time (easily parallelized)
-    y = torch.empty(outsize)
-    for i in range(outsize[0]):
+    y = torch.empty(nrows, ncols)
+    for i in range(nrows):
         # elementwise portion of softmax, one row of qkt
         qkti = np.exp(qkt[i] - qkti_max[i]) / sum_of_exp[i]
-        for j in range(outsize[1]):
+        for j in range(ncols):
             # vector dot product with column of v
-            y[i][j] = np.vdot(qkti, v[:,j]).item()
+            y[i][j] = np.vdot(qkti, v[:, j]).item()
 
     # all done :)
     return y
 
 
-def my_sdp_attn(q, k, v) -> torch.tensor:
+def my_sdp_attn(q: torch.tensor, k: torch.tensor, v: torch.tensor) -> torch.tensor:
     q = q / np.sqrt(embed_dim)
     qkt = torch.matmul(q, k.transpose(-2, -1))
     return partially_fused_softmax(qkt, v)
 
 
 # see "Attention Is All You Need"
-def gold_sdp_attn(q, k, v) -> torch.tensor:
+def gold_sdp_attn(q: torch.tensor, k: torch.tensor, v: torch.tensor) -> torch.tensor:
     # SDP assumes a batch dimension, but we don't need it for this exercise
     sdp_attn = torchtext.nn.ScaledDotProduct(batch_first=True)
     y, _ = sdp_attn(q.unsqueeze(0), k.unsqueeze(0), v.unsqueeze(0))
@@ -68,6 +68,14 @@ with torch.inference_mode():
     print(F'attn_output_golden:\n{attn_output_golden}')
     attn_output = my_sdp_attn(query, key, value)
     print(F'attn_output:\n{attn_output}')
-    assert(np.allclose(attn_output_golden, attn_output))
+    rtol, atol = 1e-05, 1e-07
+    if not np.allclose(attn_output_golden, attn_output, rtol=rtol, atol=atol):
+        for row in range(attn_output_golden.size(-2)):
+            if not np.allclose(attn_output_golden[row, :], attn_output[row, :], rtol=rtol, atol=atol):
+                for col in range(attn_output_golden.size(-1)):
+                    if not np.allclose(attn_output_golden[row, col], attn_output[row, col], rtol=rtol, atol=atol):
+                        print(F'not close gold={attn_output_golden[row, col]}, mine={attn_output[row, col]}')
+        assert False
+
 
 
